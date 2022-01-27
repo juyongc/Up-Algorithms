b87bff3bd1ce2aee5d055b1400ae07ba4421e65a
juyongc <chunjy2147@gmail.com>
2022-01-27 14:23:04 +0900

    Create PROG_없는숫자더하기.java

---
+++ PROG_없는숫자더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for (int i=0;i<10;i++) {
+            answer += i;
+        }
+        for (int num : numbers) {
+            answer -= num;
+        }
+        
+        return answer;
+    }
+}
