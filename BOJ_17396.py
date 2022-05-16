82a31f8f1e3d374b9c9d02a8c4924848b57e06e8
juyongc <chunjy2147@gmail.com>
2022-05-16 15:44:08 +0900

    Update BOJ_17396.py

--- BOJ_17396.py
+++ BOJ_17396.py
@@ -3,29 +3,37 @@ import heapq
 inputs = sys.stdin.readline
 
 N,M = map(int,inputs().split())
-discover = list(map(int,inputs().split()))
-discover[-1] = 0
-road = [{} for _ in range(N)]       # 길 저장용
+discover = list(map(int,input().split()))
+move = [{} for _ in range(N)]
+
+# 양뱡향
 for _ in range(M):
     a,b,t = map(int,inputs().split())
-    road[a][b] = t
-    road[b][a] = t
+    move[a][b] = t
+    move[b][a] = t
 
-weight = [10**15] * N
-hq = []
-heapq.heappush(hq,(0,0))
-# 최소 시간 찾기(다익스트라)
+W = [10**20] * N
+hq = [(0,0)]
+W[0] = 0
+
+# 넥서스를 제외한 발각되는 곳 W값 0으로 변경
+for i in range(N-1):
+    if discover[i] == 1:
+        W[i] = 0
+
+# 다익스트라
 while hq:
-    w,node = heapq.heappop(hq)
-    if w > weight[node]:
+    cost, now = heapq.heappop(hq)
+    if cost > W[now]:
         continue
 
-    for key,val in road[node].items():
-        if weight[key] > w + val and discover[node] == 0:
-            weight[key] = w + val
-            heapq.heappush(hq,(w + val,key))
+    for key,val in move[now].items():
+        if W[key] > cost + val:
+            W[key] = cost + val
+            heapq.heappush(hq, (cost+val, key))
 
-answer = weight[-1]
-if answer == 10**15:
+answer = W[-1]
+if answer == 10**20:
     answer = -1
+
 print(answer)
