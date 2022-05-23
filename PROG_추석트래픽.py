0144a0eb33d578537d6de98adf194c6e8192c3c3
juyongc <chunjy2147@gmail.com>
2022-05-23 20:13:34 +0900

    Update PROG_추석트래픽.py

--- PROG_추석트래픽.py
+++ PROG_추석트래픽.py
@@ -1,22 +1,51 @@
 def solution(lines):
     answer = 0
-    periods = []
-    
-    # 숫자 추출
-    for line in lines:
-        date,time,duration = line.split()
-        h,m,s = map(float,time.split(":"))
-        end_time = int((s + m * 60 + h * 3600) * 1000)
-        duration = duration.replace("s","")
-        start_time = end_time - int(float(duration)*1000) + 1
-        periods.append((start_time,end_time))
-    
-    # 현재 end_time > 다음 start_time - 999 이면 타임라인에 미포함됨
-    for i in range(len(periods)):
-        ts = periods[i][1] 
+    se = []
+    # 시간만 가져와서 float형식으로 계산
+    for li in lines:
+        dates,start,dur = li.split()
+        hh,mm,ss = start.split(":")
+        e_time = 3600.0*float(hh) + 60.0*float(mm) +float(ss)
+        s_time = e_time - float(dur[:-1]) + float(0.001)
+        se.append([s_time,e_time])
+    # 시작 시간 ~ 1초
+    for i in range(len(lines)):
         cnt = 0
-        for j in range(i,len(periods)):
-            if periods[j][0] - 999 <= ts:
-                cnt += 1
+        fs,fe = se[i][0],round(se[i][0] + 0.999,3)
+        for j in range(len(lines)):
+            cur_s,cur_e = se[j]
+            if cur_e < fs or fe < cur_s:
+                continue
+            cnt += 1
+        answer = max(answer,cnt)
+    # 시작 시간 - 1 ~ 시작 시간
+    for i in range(len(lines)):
+        cnt = 0
+        fs,fe = round(se[i][0] - 0.999,3),se[i][0]
+        for j in range(len(lines)):
+            cur_s,cur_e = se[j]
+            if cur_e < fs or fe < cur_s:
+                continue
+            cnt += 1
+        answer = max(answer,cnt)
+    # 완료 시간 - 1 ~ 완료 시간
+    for i in range(len(lines)):
+        cnt = 0
+        bs,be = round(se[i][1] - 0.999,3),se[i][1] 
+        for j in range(len(lines)):
+            cur_s,cur_e = se[j]
+            if cur_e < bs or be < cur_s:
+                continue
+            cnt += 1
+        answer = max(answer,cnt)
+    # 완료 시간 ~ 완료 시간 + 1
+    for i in range(len(lines)):
+        cnt = 0
+        bs,be = se[i][1],round(se[i][1]  + 0.999,3)
+        for j in range(len(lines)):
+            cur_s,cur_e = se[j]
+            if cur_e < bs or be < cur_s:
+                continue
+            cnt += 1
         answer = max(answer,cnt)
     return answer
